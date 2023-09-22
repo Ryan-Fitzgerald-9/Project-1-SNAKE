27d762003e88f10b3c96cce408bb3cf318839424
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-09-22 15:32:09 -0400

    initial commitclear

---
+++ script.js
@@ -0,0 +1,25 @@
+// Define variables (snake, dot, scoreboard...)
+
+
+// Snake movement, create out-of-bounds
+
+
+// Logic for dot (scoring and then random location)
+
+
+// Adding blocks to end of snake after each point
+
+
+// Game Over logic (moving into wall or itself)
+
+
+// Creating difficulty settings (smaller blocks and faster movement?)
+
+
+// Functions for Scoring, Game Menu, Start Game, and Game Over
+
+
+// Event listener for arrow keys
+
+
+
