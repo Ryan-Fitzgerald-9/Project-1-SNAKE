1f19fcc4410385720da01fe053120d9884803e48
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-09-24 19:57:28 -0400

    named most vars and funct needed

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Define variables (snake, dot, scoreboard...)
+const gameBoard = document.querySelector('#game-area')
+const ctx = gameBoard.getContext("2d")
+const scoreCurrent = document.querySelector('#score-current')
+const scoreHigh = document.querySelector('#score-high')
+const startBtn = document.querySelector('#start-button')
+const resetBtn = document.querySelector('#restart-button')
+const modeBtn = document.querySelector('#difficulty')
+
+const gameWidth = gameBoard.width
+const gameHeight = gameBoard.height
+
+let gameSpeed = 1
+
 
 
 // Snake movement, create out-of-bounds
@@ -17,6 +30,17 @@
 
 
 // Functions for Scoring, Game Menu, Start Game, and Game Over
+const startGame = () => {}
+const clearBoard = () => {}
+const nextTick = () => {}
+const createFood = () => {}
+const drawFood = () => {}
+const moveSnake = () => {}
+const drawSnake = () => {}
+const changeDirection = () => {}
+const checkGameOver = () => {}
+const displayGameOver = () => {}
+const resetGame = () => {}
 
 
 // Event listener for arrow keys
