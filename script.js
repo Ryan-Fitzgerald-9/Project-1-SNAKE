300da63f11c54b3fd1df4ffc5b63a2594e05d89b
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-09-26 14:42:57 -0400

    created modal

--- script.js
+++ script.js
@@ -238,6 +238,27 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
+//Grabbing Elements
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal')
+const close = document.getElementById('close')
+
+//Functions
+const openModal = () => {
+  modal.style.display = 'block';
+}
+
+const closeModal = () => {
+  modal.style.display = 'none'
+}
+
+//Event Listeners
+openBtn.addEventListener('click', openModal)
+
+close.addEventListener('click', closeModal)
+
+
+
 
 
 
